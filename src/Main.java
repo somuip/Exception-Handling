555d2bdef29172db65acace65ca9892fc0d9ea5b
somuip <117620797+somuip@users.noreply.github.com>
2023-02-20 23:26:05 +0530

    custom exception:

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,49 @@
 public class Main {
     public static void main(String[] args) {
 
-//		System.out.println("A");
-//		int a = 1;
-//		try {
-//			int i = 5/0;
-//			System.out.println("B");
-//		}
-////		catch(Exception e) {          // wrong
-////			System.out.println("P");
-////		}
-//		catch(ArrayIndexOutOfBoundsException e) {
-//			System.out.println("D");
-//		}
-//		catch(ArithmeticException e) {
-//			System.out.println("R");
-//		}
-//		catch(Exception e) {
-//			System.out.println("P");
-//		}
-//		finally {
-//			System.out.println("S");
-//		}
-//		System.out.println("C");
-
-        // throw keyword
         System.out.println("A");
-        try {
+//        int arr[] = {1,2,3,4};
+//
+//        try{
+//            System.out.println(arr[6]);
+//        }
+//        catch(ArithmeticException e){
+//            System.out.println("C");
+//        }
+//        finally{
+//            System.out.println("D");
+//        }
+//        System.out.println("E");
+//
+//        System.out.println("A");
+        try{
             fun(5);
         }
-        catch(Exception e) {
-            System.out.println("Your not eligible "+e);
+        catch (Exception e){
+            System.out.println("Chhoti bacchi ho kya "+e);
         }
-    }
+        System.out.println("B");
 
-//    public static int fun(int age) {
-//        if(age < 18) {
-//            throw new RuntimeException("Under age");
+//        try{
+//        fun1();
 //        }
-//        return 10;
-//    }
+//        catch(ArithmeticException e){
+//            System.out.println("A");
+//        }
 
-    public static void fun(int age) throws ArithmeticException, ArrayIndexOutOfBoundsException{
-        int i = 5/0;
     }
-}
 
-// Exception class is the parent so it should come after child class
-// finally block will come with try catch and it should always come after try catch, and it definitely execute
+    public static int fun(int age) throws UnderAgeException {
+
+        if(age<18){
+            throw new UnderAgeException();
+        }
+        return 10;
+    }
+
+//    public static void fun1() throws Exception{
+//        int i = 5/0;
+//    }
+}
+// Exception class is the parent, so it should come after child class
+// finally block will come with try catch, and it should always come after try catch, and it definitely execute
